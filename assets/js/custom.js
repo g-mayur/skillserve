cc54548af13255fc1ff7dbf240d0e275c96afaa1
anamika-88 <anamikap.crestinfosystems@gmail.com>
2024-06-03 15:34:29 +0530

    Design the header & footer responsive & worked on home page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,23 @@
 $(document).ready(function () {
 
   //responsive header menu js start
-  $(".cl__toggle-menu").click(function () {
-    $(".cl__menu-list").toggleClass("cl__menu-active");
-  });
-  $(".cl__toggle-menu").click(function () {
-    $("body").toggleClass("cl__menu-open");
+
+  $(".navbar-toggler").click(function () {
+    $("body").toggleClass("cs__menu-open");
   });
 
-  // Customer Experiences slider start
+  // Responsive header menu JS end
 
-  var swiper = new Swiper(".mySwiper.cl__cust-slider", {
-    slidesPerView: 1,
-    spaceBetween: 30,
+  // Hero section slider start
+
+  var swiper = new Swiper(".mySwiper.cs__hero-slider", {
+    direction: "vertical",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    autoplay: {
-      delay: 3000,
-    },
-    breakpoints: {
-      // when window width is >= 320px
-      767: {
-        slidesPerView: 2,
-        spaceBetween: 30
-      },
-    }
-    
   });
+
+    // Hero section slider end
 
 });
